1a1fe2fed42e46c76402f4eda88398a5602fc37b
Richard Hounslow <richard.hounslow@sportsbet.com.au>
2022-01-27 16:18:48 +1100

    updated app.js add v6 links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Link} from "react-router-dom"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Link
+                    className="App-link"
+                    to="/header"
+                >
+                    MUI Drawer with Header
+                </Link>
+                <Link
+                    className="App-link"
+                    to="/noheader"
+                >
+                    MUI Drawer without Header
+                </Link>
+            </header>
+        </div>
+    );
 }
 
 export default App;
